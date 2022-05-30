0421ba0b66a34cec6fdfc8bb72e5fcb87eb61779
Marek Fleischmann <marek.fleischmann@educhem.cz>
2022-05-30 08:05:40 +0200

    Přidejte soubory projektu.

---
+++ Brezen/Program.cs
@@ -0,0 +1,12 @@
+﻿int cislo1 = Convert.ToInt32(Console.ReadLine());
+int cislo2 = Convert.ToInt32(Console.ReadLine());
+if (cislo1 > cislo2)
+{
+    Console.WriteLine(cislo1);
+    Console.WriteLine(cislo2);
+}
+else
+{
+    Console.WriteLine(cislo2);
+    Console.WriteLine(cislo1);
+}
